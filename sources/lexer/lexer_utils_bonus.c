6545aed1a940bfc051460a2a38e26a03848e4015
Lila Fallet <lfallet@student.42.fr>
2020-09-08 10:40:40 +0200

    [DEV] add handle assign [NEXT] finish handle quote

--- sources/lexer/lexer_utils_bonus.c
+++ sources/lexer/lexer_utils_bonus.c
@@ -1,7 +1,7 @@
 #include "minishell_bonus.h"
 
 void	exit_routine_lexer(t_vector *word, t_vector *vct, t_vector *tmp,
-							t_token *token, t_list *node)
+		t_token *token, t_list *node)
 {
 	if (word != NULL)
 		vct_del(&word);
@@ -21,12 +21,31 @@ void	exit_routine_lexer(t_vector *word, t_vector *vct, t_vector *tmp,
 	}
 }
 
+int	handle_assign(t_vector *vct, t_vector *input, t_list **token_list)
+{
+	t_vector	*tmp;
+	int			ret_extract;
+	size_t		len_tmp;
+	
+	tmp = vct_new();
+	tmp = vct_cdup(input, ' ');
+	len_tmp = vct_getlen(tmp);
+	vct = vct_join(vct, tmp);
+	ret_extract = extract_token(token_list, vct_getstr(vct), E_ASSIGN);
+	if (ret_extract == FAILURE)
+	{
+		exit_routine_lexer(NULL, vct, tmp, NULL, NULL);
+		free_list(token_list);
+		return (FALSE);
+	}
+	vct_popfrom(input, len_tmp);
+	return (TRUE);
+}
+
 int	handle_quote(t_vector *input, t_list **token_list, int ret)
 {
 	t_vector	*vct;
 	size_t		len;
-	t_vector	*tmp;
-	size_t		len_tmp;
 	int			ret_extract;
 
 	ret_extract = SUCCESS;
@@ -45,23 +64,14 @@ int	handle_quote(t_vector *input, t_list **token_list, int ret)
 	vct_popfrom(input, len - 1);
 	if (vct_getcharat(input, FIRST_CHAR) == '=')
 	{
-		tmp = vct_new();
-		tmp = vct_cdup(input, ' ');
-		len_tmp = vct_getlen(tmp);
-		vct = vct_join(vct, tmp);
-		ret_extract = extract_token(token_list, vct_getstr(vct), E_ASSIGN);
-		if (ret_extract == FAILURE)
-		{
-			exit_routine_lexer(NULL, vct, tmp, NULL, NULL);
-			free_list(token_list);
+		ret_extract = handle_assign(vct, input, token_list);
+		if (ret_extract == FALSE)
 			return (FALSE);
-		}
-		vct_popfrom(input, len_tmp);
 	}
 	else
 	{
 		ret_extract = extract_token(token_list, vct_getstr(vct), ret == N_SIMPLE_QUOTE
-						? E_SIMPLE_QUOTE : E_QUOTE);
+				? E_SIMPLE_QUOTE : E_QUOTE);
 		if (ret_extract == FAILURE)
 		{
 			exit_routine_lexer(NULL, vct, NULL, NULL, NULL);
