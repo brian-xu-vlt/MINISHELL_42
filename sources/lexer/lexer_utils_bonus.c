b33c03fe42ae291042de9a5449d8f9a7041be70f
Lila Fallet <lfallet@student.42.fr>
2020-09-08 10:56:17 +0200

    [DEV] handle quote norme [NEXT] lexer

--- sources/lexer/lexer_utils_bonus.c
+++ sources/lexer/lexer_utils_bonus.c
@@ -26,7 +26,7 @@ int	handle_assign(t_vector *vct, t_vector *input, t_list **token_list)
 	t_vector	*tmp;
 	int			ret_extract;
 	size_t		len_tmp;
-	
+
 	tmp = vct_new();
 	tmp = vct_cdup(input, ' ');
 	len_tmp = vct_getlen(tmp);
@@ -42,15 +42,28 @@ int	handle_assign(t_vector *vct, t_vector *input, t_list **token_list)
 	return (TRUE);
 }
 
+int	no_assign(t_list **token_list, t_vector *vct, int ret)
+{
+	int	ret_extract;
+
+	ret_extract = extract_token(token_list, vct_getstr(vct), ret == N_SIMPLE_QUOTE
+			? E_SIMPLE_QUOTE : E_QUOTE);
+	if (ret_extract == FAILURE)
+	{
+		exit_routine_lexer(NULL, vct, NULL, NULL, NULL);
+		free_list(token_list);
+		return (FALSE);
+	}
+	return (TRUE);
+}
+
 int	handle_quote(t_vector *input, t_list **token_list, int ret)
 {
 	t_vector	*vct;
 	size_t		len;
 	int			ret_extract;
 
-	ret_extract = SUCCESS;
-	vct = vct_cdup(input, ret == N_SIMPLE_QUOTE ? C_SIMPLE_QUOTE
-			: C_QUOTE);
+	vct = vct_cdup(input, ret == N_SIMPLE_QUOTE ? C_SIMPLE_QUOTE : C_QUOTE);
 	if (vct_equ(input, vct) == TRUE)
 	{
 		vct_del(&vct);
@@ -63,22 +76,9 @@ int	handle_quote(t_vector *input, t_list **token_list, int ret)
 	len = vct_getlen(vct);
 	vct_popfrom(input, len - 1);
 	if (vct_getcharat(input, FIRST_CHAR) == '=')
-	{
 		ret_extract = handle_assign(vct, input, token_list);
-		if (ret_extract == FALSE)
-			return (FALSE);
-	}
 	else
-	{
-		ret_extract = extract_token(token_list, vct_getstr(vct), ret == N_SIMPLE_QUOTE
-				? E_SIMPLE_QUOTE : E_QUOTE);
-		if (ret_extract == FAILURE)
-		{
-			exit_routine_lexer(NULL, vct, NULL, NULL, NULL);
-			free_list(token_list);
-			return (FALSE);
-		}
-	}
+		ret_extract = no_assign(token_list, vct, ret);
 	vct_del(&vct);
-	return (TRUE);
+	return (ret_extract);
 }
