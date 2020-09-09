83b4448d13b9b1be9fbb173939a9ee4f3b18f771
Lila Fallet <lfallet@student.42.fr>
2020-09-09 18:45:23 +0200

    [DEV] set all tab in all function [NEXT] begin algo

--- sources/parser/parser_bonus.c
+++ sources/parser/parser_bonus.c
@@ -1,29 +1,58 @@
 #include "minishell_bonus.h"
 
-static void	tab_redirect(enum e_token_type *valid_token)
+static void	tab_redirect(t_valid_token *token)
 {
+	static enum e_token_type	tab_valid[] = {E_SIMPLE_QUOTE, E_QUOTE,
+												E_WORD, E_EXP, E_ASSIGN};
+	
+	token[E_LESS_THAN].next_token = tab_valid;
+	token[E_GREATER_THAN].next_token = tab_valid;
+	token[E_DOUBLE_GREATER].next_token = tab_valid;
 
 }
 
-static void	tab_word_exp_assign(enum e_token_type *valid_token)
+static void	tab_word_exp_assign(t_valid_token *token)
 {
+	static enum e_token_type	tab_valid[] = {E_SEPARATOR, E_PIPE,
+												E_SIMPLE_QUOTE, E_QUOTE,
+												E_LESS_THAN, E_GREATER_THAN,
+												E_DOUBLE_GREATER, E_OR, E_AND,
+												E_WORD, E_EXP, E_ASSIGN, E_END};
 
+	token[E_SIMPLE_QUOTE].next_token = tab_valid;
+	token[E_QUOTE].next_token = tab_valid;
+	token[E_WORD].next_token = tab_valid;
+	token[E_EXP].next_token = tab_valid;
+	token[E_ASSIGN].next_token = tab_valid;
 }
 
-static void	tab_pipe_or_and(enum e_token_type *valid_token)
+static void	tab_pipe_or_and(t_valid_token *token)
 {
-
+	static enum e_token_type	tab_valid[] = {E_SIMPLE_QUOTE, E_QUOTE,
+												E_LESS_THAN, E_GREATER_THAN,
+												E_DOUBLE_GREATER, E_WORD,
+												E_EXP, E_ASSIGN};
+	
+	token[E_PIPE].next_token = tab_valid;
+	token[E_OR].next_token = tab_valid;
+	token[E_AND].next_token = tab_valid;
 }
 
-static void	tab_start_separator(enum e_token_type *valid_token)
+static void	tab_start_separator(t_valid_token *token)
 {
-
+	static enum e_token_type	tab_valid[] = {E_SIMPLE_QUOTE, E_QUOTE,
+												E_LESS_THAN, E_GREATER_THAN,
+												E_DOUBLE_GREATER, E_WORD,
+												E_EXP, E_ASSIGN, E_END};
+	
+	token[E_SEPARATOR].next_token = tab_valid;
+	token[E_START].next_token = tab_valid;
 }
 
 t_btree 	*parser_token(t_list *token_list)
 {
 	t_btree	*ast;
-	static enum e_token_type valid_token[NB_TOKEN];
+	static t_valid_token valid_token[NB_TOKEN];
 
 	ast = NULL;
 	ft_bzero(&valid_token, sizeof(valid_token));
@@ -31,6 +60,5 @@ t_btree 	*parser_token(t_list *token_list)
 	tab_pipe_or_and(valid_token);
 	tab_word_exp_assign(valid_token);
 	tab_redirect(valid_token);
-	ft_printf("HEY CA PASSE\n");//DEBUG
 	return (ast);
 }
