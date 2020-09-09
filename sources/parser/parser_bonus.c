ca5207e1a09a555f31b0aca1199a5279c8f2914c
Lila Fallet <lfallet@student.42.fr>
2020-09-09 18:19:33 +0200

    [DEV] set different function [NEXT] create tab in diff function

--- sources/parser/parser_bonus.c
+++ sources/parser/parser_bonus.c
@@ -1,10 +1,36 @@
 #include "minishell_bonus.h"
 
-t_btree *parser_token(t_list *token_list)
+static void	tab_redirect(enum e_token_type *valid_token)
+{
+
+}
+
+static void	tab_word_exp_assign(enum e_token_type *valid_token)
+{
+
+}
+
+static void	tab_pipe_or_and(enum e_token_type *valid_token)
+{
+
+}
+
+static void	tab_start_separator(enum e_token_type *valid_token)
+{
+
+}
+
+t_btree 	*parser_token(t_list *token_list)
 {
 	t_btree	*ast;
+	static enum e_token_type valid_token[NB_TOKEN];
 
 	ast = NULL;
+	ft_bzero(&valid_token, sizeof(valid_token));
+	tab_start_separator(valid_token);
+	tab_pipe_or_and(valid_token);
+	tab_word_exp_assign(valid_token);
+	tab_redirect(valid_token);
 	ft_printf("HEY CA PASSE\n");//DEBUG
 	return (ast);
 }
