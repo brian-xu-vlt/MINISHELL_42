1929c0415839626d9495473713fa8ae459dc1e7f
Lila Fallet <lfallet@student.42.fr>
2020-10-09 23:13:46 +0200

    [FIX] try to fix bug [NEXT] fix bug on cmd->name

--- sources/command_job/job_command_fill_bonus.c
+++ sources/command_job/job_command_fill_bonus.c
@@ -4,8 +4,10 @@ void	fill_name(char *str, t_cmd *cmd)
 {
 	char	*cpy_name;
 
+	ft_printf("CMD->NAME FIRST = %s\n", cmd->name);//DEBUG
 	cpy_name = ft_strjoin(cmd->name, str);
 	cmd->name = ft_strjoin(cpy_name, " ");	
+	ft_printf("CMD->NAME END = %s\n", cmd->name);//DEBUG
 	free(cpy_name);
 }
 
