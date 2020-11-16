5772f70300dbcf21686ec00fb4dc9d4780d8c889
Lila Fallet <lfallet@student.42.fr>
2020-11-16 20:40:46 +0100

    [16.11-20:40] fix lexer quote [NEXT] start exit

--- sources/lexer/token_bonus.c
+++ sources/lexer/token_bonus.c
@@ -54,9 +54,9 @@ int			extract_token(t_list **token_list, char *str, size_t type)
 		return (FAILURE);
 	}
 	token->type = type;
-	//ft_printf("token->data = %s\n", token->data); //DEBUG
-	//debug(token->type);//DEBUG
-	//ft_printf("\n");//DEBUG
+	ft_printf("token->data = %s\n", token->data); //DEBUG
+	debug(token->type);//DEBUG
+	ft_printf("\n");//DEBUG
 	node = ft_lstnew(token);
 	if (node == NULL)
 	{
