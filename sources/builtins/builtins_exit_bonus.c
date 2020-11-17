3ae82f82771f0b0c7def0a965b54d1a9cdc9b0b7
Lila Fallet <lfallet@student.42.fr>
2020-11-17 17:06:38 +0100

    [17.11-17:06] finish exit [NEXT] fix when exp with quote

--- sources/builtins/builtins_exit_bonus.c
+++ sources/builtins/builtins_exit_bonus.c
@@ -47,34 +47,54 @@ static bool	parse_vct(t_vector *vct_av)
 	while (vct_getlen(av) > 0)
 	{
 		if (vct_getfirstchar(av) != C_SPACE && vct_getfirstchar(av) != C_TAB)
+		{
+			vct_del(&av);
 			return (false);
+		}
 		vct_pop(av);
 	}
+	vct_del(&av);
 	return (true);
 }
 
-static bool	is_to_long(t_vector *av, char c)
+static bool	is_long(t_vector *av, char c)
 {
-	if ((c == C_MINUS || c == C_PLUS) && vct_getlen(av) > LEN_MIN_LONG)
-		return (false);
-	else if (c != C_MINUS && c != C_PLUS &&vct_getlen(av) > LEN_PLUS_LONG)
-		return (false);
-	if (c == C_PLUS)
-		vct_pop(av);
-	ft_printf("av = %s\n", vct_getstr(av));//DEBUG
+	static char	*ref_plus = "9223372036854775807";
+	static char	*ref_minus = "9223372036854775808";
+	size_t	i;
+	char	first_c;
+	char	*str;
 
-	return (true);
+	i = 0;
+	if (vct_getlen(av) > 19 && vct_getfirstchar(av) != '0') 
+		return (true);
+	else if (vct_getlen(av) < 19)
+		return (false);
+	str = c == C_MINUS ? ref_minus : ref_plus;
+	while (vct_getlen(av) > 0 && str[i] != '\0')
+	{
+		first_c = vct_getfirstchar(av);
+		if (i == 0 && first_c < '9')
+			return (false);
+		if (first_c == '0' && str[i] != '0')
+			return (false);
+		if (first_c > str[i])
+			return (true);
+		i++;
+		vct_pop(av);
+	}
+	return (false);
 }
 
 int	exit_builtin(int ac, char **av, char **envp)
 {
-	int	exit_value;
+	uint8_t	exit_value;
 	t_vector	*vct_av;
 	t_vector	*vct_av_cpy;
 	bool			num_arg;
 	char		c;
 	bool		ret_parser;
-	bool		ret_to_long;
+	bool		ret_long;
 
 	ft_printf("EXEC BUILTIN\n"); //DEBUG
 	(void)envp;
@@ -82,8 +102,7 @@ int	exit_builtin(int ac, char **av, char **envp)
 	if (ac == 1)
 	{
 		ft_printf("%s\n", EXIT);
-		ft_printf("\n");//DEBUG
-		//exit(get_env_value_int(get_env_list(GET), S_QUESTION_MARK));
+		exit(get_env_value_int(get_env_list(GET), S_QUESTION_MARK));
 		return (EXIT_FAIL);
 	}
 	vct_av = vct_new();
@@ -99,46 +118,52 @@ int	exit_builtin(int ac, char **av, char **envp)
 	num_arg = is_numeric(vct_av_cpy);
 	if (num_arg == false || vct_getlen(vct_av) == 0)
 	{
-		ft_printf("vct_ac = %s\n", vct_getstr(vct_av));//DEBUG
 		ft_printf("%s\n", EXIT);
 		if (vct_getlen(vct_av) != 0)
 			print_set_errno(0, "numeric argument required", EXIT, av[1]);
 		else
 			print_set_errno(0, "numeric argument required", EXIT,
 								c == C_PLUS ? S_PLUS : S_MINUS);
-		ft_printf("\n");//DEBUG
+		vct_del(&vct_av);
+		exit(2);	
 		return (EXIT_FAIL);
-		//exit(2);	
 	}
 	ret_parser = parse_vct(vct_av);
-	ft_printf("vct_av AFTER PARSE = [%s]\n", vct_getstr(vct_av));//DEBUG
 	if (ret_parser == false)
 	{
 		ft_printf("%s\n", EXIT);
 		print_set_errno(0, "numeric argument required", EXIT,
 							av[1]);
-		ft_printf("\n");//DEBUG
+		vct_del(&vct_av);
+		exit(2);	
 		return (EXIT_FAIL);
-		//exit(2);	
-
 	}
-	ft_printf("c = %c\n", c);//DEBUG
+	vct_cpy(vct_av_cpy, vct_av);
+	ret_long = is_long(vct_av, c);
+	if (ret_long == true)
+	{
+		ft_printf("%s\n", EXIT);
+		print_set_errno(0, "numeric argument required", EXIT,
+							av[1]);
+		vct_del(&vct_av);
+		exit(2);	
+		return (EXIT_FAIL);
+	}
+	if (ret_long == false && ac > 2)
+	{
+		ft_printf("%s\n", EXIT);
+		print_set_errno(0, "too many arguments", EXIT,
+							av[1]);
+		vct_del(&vct_av);
+		exit(2);	
+		return (EXIT_FAIL);
+	}
+	vct_cpy(vct_av, vct_av_cpy);
 	if (c == C_MINUS || c == C_PLUS)
 		vct_addcharat(vct_av, 0, c);
-	ft_printf("vct_av AFTER ADD = [%s]\n", vct_getstr(vct_av));//DEBUG
-	ret_to_long = is_to_long(vct_av, c);
-	ft_printf("ret_to_long = %d\n", ret_to_long);//DEBUG
-	ft_printf("\n");//DEBUG
-	//exit(exit_value);
+	exit_value = ft_atoi(vct_getstr(vct_av));
+	vct_del(&vct_av);
+	ft_printf("%s\n", EXIT);
+	exit(exit_value);
 	return (SUCCESS);
 }
-	
-	
-	/*const char	*builtin = "exit";
-
-	(void)envp;
-	if (ft_strequ(av[0], (char *)builtin) == FALSE)
-		return (0);
-	if (ac == 1)
-		exit_routine_le("exit");
-	return (SUCCESS);*/
