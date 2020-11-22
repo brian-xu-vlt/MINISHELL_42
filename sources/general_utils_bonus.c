06023097b53d1a4efe51e2519b4fe9f9d6936023
Brian on VM <bvalette@student.42.fr>
2020-11-22 09:45:19 +0100

    [Sun-22.11-09:45] [DEV] check malloc source/executor [NEXT]

--- sources/general_utils_bonus.c
+++ sources/general_utils_bonus.c
@@ -14,5 +14,7 @@ void		free_char_arr(char **arr)
 
 int		is_path(const char *bin_name)
 {
+	if (bin_name == NULL)
+		return (false);
 	return ((ft_strchr((char *)bin_name, '/') == NOT_FOUND) ? FALSE : TRUE);
 }
