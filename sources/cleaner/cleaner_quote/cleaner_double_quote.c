7fa11544c0c65f390e6e4068789bcae8c5aaab20
Lila Fallet <lfallet@student.42.fr>
2020-12-03 16:21:54 +0100

    [03.12-16:21] add header lila [NEXT] merge

--- sources/cleaner/cleaner_quote/cleaner_double_quote.c
+++ sources/cleaner/cleaner_quote/cleaner_double_quote.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleaner_double_quote.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 16:16:29 by lfallet           #+#    #+#             */
+/*   Updated: 2020/12/03 16:16:30 by lfallet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 static int	process_parse_double_quote(char c, char next_c, t_vector *input,
